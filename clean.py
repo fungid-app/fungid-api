086b57992ddebe6f175b23e20a2ccd4ebf521577
Michael Weishuhn <weishuhn@gmail.com>
2022-05-17 16:42:46 +0100

    Added batch categorization

--- clean.py
+++ clean.py
@@ -4,7 +4,7 @@ from fastai.data.external import *
 import ntpath
 import csv
 
-learn_inf = load_learner('models/clean-res18.pkl')
+learn_inf = load_learner('models/clean-res18.pkl', cpu=True)
 d = set()
 
 with open("categorized.csv", "r") as f:
@@ -16,25 +16,52 @@ print("found {} categorized images".format(len(d)))
 
 inserted = 0
 
+batch = []
+
+
+def splitFileName(file):
+    _, tail = ntpath.split(file)
+    id = tail.replace(".png", "")
+    parts = id.split("-")
+    return id, parts
+
+
+def predictBatch(tst_files, output):
+    test_dl = learn_inf.dls.test_dl(tst_files)
+    preds, _, decoded = learn_inf.get_preds(
+        dl=test_dl, with_decoded=True, reorder=False)
+
+    for i, pred in enumerate(preds):
+        _, parts = splitFileName(tst_files[i])
+        confidence = pred[decoded[i]]
+        percent = float(confidence)
+        label = learn_inf.dls.vocab[decoded[i]]
+        output.write( parts[0] + "," + parts[1] + ',' +
+              label + ',' + str(percent) + "\n")
+
+
 with open("categorized.csv", "a") as f:
     for i, line in enumerate(fileinput.input()):
-        inputfile = line.strip()
-        _, tail = ntpath.split(inputfile)
-        id = tail.replace(".png", "")
-        parts = id.split("-")
+        file = line.strip()
+        id, parts = splitFileName(file)
 
-        if i % 1000 == 0:
+        if i % 10000 == 0:
             print("{}/{}".format(inserted, i))
 
         if id not in d:
-            try:
-                img = Image.open(inputfile)
-                pred, pred_idx, probs = learn_inf.predict(inputfile)
-                confidence = probs[pred_idx]
-                percent = float(confidence)
-                f.write(parts[0] + "," + parts[1] + "," + pred + "," + str(percent) + "\n")
-                inserted += 1
-            except:
-                print(inputfile + ",notfound")
+            batch.append(file)
+            inserted += 1
+
+        if len(batch) > 10000:
+            print("predicting batch")
+            predictBatch(batch, f)
+            print("done")
+            batch = []
+
+    if len(batch) > 0:
+        print("predicting batch")
+        predictBatch(batch, f)
+        print("done")
+        batch = []
 
  
