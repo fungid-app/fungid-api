c7e1e446207e60c778318528376ccf76ba54c1c0
weishuhn <weishuhn@gmail.com>
2022-06-16 10:00:02 +0100

    Image Classification and raster files

--- resize.py
+++ resize.py
@@ -10,14 +10,21 @@ rsz = Resize(newsize, ResizeMethod.Pad, pad_mode='zeros')
 
 def resize_image(line):
     inputfile = line.strip()
-    outputfile = inputfile.replace("500", str(newsize))
+    outputfile = inputfile.replace("/500/", '/' + str(newsize) + '/')
 
     if not exists(outputfile):
-        img = PILImage.create(inputfile)
-        img = rsz(img)
-        img.save(inputfile.replace("500", str(newsize)))
+        try:
+            img = PILImage.create(inputfile)
+            img = rsz(img)
+            img.save(outputfile)
+            print("resized file: ", outputfile)
+        except Exception as e:
+            print(e)
+            print(inputfile)
+    else:
+        print("did not resize file: ", outputfile)
 
 
 if __name__ == "__main__":
-    with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
+    with concurrent.futures.ThreadPoolExecutor(max_workers=64) as executor:
         executor.map(resize_image, fileinput.input())
