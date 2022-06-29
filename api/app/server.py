07ea1926d7fdbf268f97c4e0e5165110b524f4c8
Michael Weishuhn <weishuhn@gmail.com>
2022-06-29 10:44:49 +0100

    starting starlette api

---
+++ api/app/server.py
@@ -0,0 +1,65 @@
+import aiohttp
+import asyncio
+import uvicorn
+from fastai import *
+from fastai.vision import *
+from fastai.vision.utils import *
+from io import BytesIO
+from starlette.applications import Starlette
+from starlette.middleware.cors import CORSMiddleware
+from starlette.responses import HTMLResponse, JSONResponse
+from starlette.staticfiles import StaticFiles
+from api.core.imageclassifier import ImageClassifier
+
+model_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_name = './export.pkl'
+
+classes = ['black', 'grizzly', 'teddys']
+
+app = Starlette()
+app.add_middleware(CORSMiddleware, allow_origins=[
+                   '*'], allow_headers=['X-Requested-With', 'Content-Type'])
+
+
+async def download_file(url, dest):
+    if dest.exists():
+        return
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            data = await response.read()
+            with open(dest, 'wb') as f:
+                f.write(data)
+
+
+async def setup_learner():
+    await download_file(export_file_url, export_file_name)
+    try:
+        learn = ImageClassifier(export_file_name)
+        return learn
+    except RuntimeError as e:
+        if len(e.args) > 0 and 'CPU-only machine' in e.args[0]:
+            print(e)
+            message = "\n\nThis model was trained with an old version of fastai and will not work in a CPU environment.\n\nPlease update the fastai library in your training environment and export your model again.\n\nSee instructions for 'Returning to work' at https://course.fast.ai."
+            raise RuntimeError(message)
+        else:
+            raise
+
+
+loop = asyncio.get_event_loop()
+tasks = [asyncio.ensure_future(setup_learner())]
+learn = loop.run_until_complete(asyncio.gather(*tasks))[0]
+loop.close()
+
+
+@app.route('/analyze', methods=['POST'])
+async def analyze(request):
+    img_data = await request.form()
+    img_bytes = await (img_data['file'].read())
+    img = PILImage.create(BytesIO(img_bytes))
+    prediction = learn.predict(img)[0]
+    return JSONResponse({'result': str(prediction)})
+
+
+if __name__ == '__main__':
+    if 'serve' in sys.argv:
+        uvicorn.run(app=app, host='0.0.0.0', port=5000, log_level="info")
--- api/core/georaster.py
+++ api/core/georaster.py
@@ -2,6 +2,7 @@ from typing import Tuple, Optional
 import pandas as pd
 import numpy as np
 import rasterio
+from rasterio.windows import Window
 import math
 import sqlite3
 
@@ -11,7 +12,32 @@ class GeoRaster:
         self.raster_path = raster_path
         self.raster_data = self.read_raster()
         self.transformer = self.get_transformer()
-        self.raster_band = self.get_band()
+
+    def find_val(self, x: int, y: int, depth: int) -> int:
+        """
+        Find the closest value at a given x, y coordinate, given a depth
+        """
+        window = Window(y-depth, x-depth, 2*depth + 1, 2*depth + 1)
+        data = self.raster_data.read(1, window=window)
+        return get_common_val(data)
+
+    def find_closest_value(self, x: int, y: int) -> int:
+        """
+        Find the closest value at a given x, y coordinate
+        """
+        val: int = self.find_val(x, y, 0)
+
+        depth: int = 1
+
+        while val == 0:
+            val = self.find_val(x, y, depth)
+            depth = math.ceil(depth * 1.25)
+
+        return val
+
+    def get_value(self, lat: float, long: float):
+        (x, y) = self.transformer.rowcol(long, lat)
+        return int(self.find_closest_value(x, y))
 
     def read_raster(self):
         """
@@ -25,42 +51,15 @@ class GeoRaster:
         """
         return rasterio.transform.AffineTransformer(self.raster_data.transform)
 
-    def get_band(self):
-        """
-        Get the raster band
-        """
-        return self.raster_data.read(1)
 
-    def find_val(self, x: int, y: int, depth: int) -> int:
-        """
-        Find the closest value at a given x, y coordinate, given a depth
-        """
-        local = self.raster_band[int(x)-depth:int(x)+depth,
-                                 int(y)-depth:int(y)+depth]
-        bins = np.bincount(local.flatten())
-        if(len(bins) > 0):
-            bins[0] = 0
-            return bins.argmax()
-        else:
-            return 0
+def get_common_val(window) -> int:
+    bins = np.bincount(window.flatten())
 
-    def find_closest_value(self, x: int, y: int) -> int:
-        """
-        Find the closest value at a given x, y coordinate
-        """
-        depth = 1
-
-        val: int = self.raster_band[x, y]
-
-        while val == 0:
-            val = self.find_val(x, y, depth)
-            depth = math.ceil(depth * 1.25)
-
-        return val
-
-    def get_value(self, lat: float, long: float):
-        (x, y) = self.transformer.rowcol(long, lat)
-        return int(self.find_closest_value(x, y))
+    if(len(bins) > 0):
+        bins[0] = 0
+        return bins.argmax()
+    else:
+        return 0
 
 
 class KGRaster:
@@ -95,7 +94,8 @@ if __name__ == "__main__":
     if kgpath is None or elupath is None:
         raise Exception("KG_RASTER_PATH and ELU_RASTER_PATH must be set")
 
-    raster = KGRaster(kgpath)
-    print(raster.get_value(52.905696, -1.225849))
-    raster = EluRaster(elupath)
-    print(raster.get_classes(52.905696, -1.225849))
+    raster = GeoRaster(elupath)
+    val = raster.get_value(52.905696, -1.225849)
+    print(val)
+    # raster = EluRaster(elupath)
+    # print(raster.get_classes(52.905696, -1.225849))
