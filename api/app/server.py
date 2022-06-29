ab7d713d31cef8ecefc126861e2e743f8c6ef43d
Michael Weishuhn <weishuhn@gmail.com>
2022-06-29 20:54:21 +0100

    Split up api and changed dockerfile

--- api/app/server.py
+++ api/app/server.py
@@ -1,69 +1,15 @@
-import aiohttp
-import asyncio
 import uvicorn
 import sys
 from datetime import datetime
-from pathlib import Path
-from fastai import *
-from fastai.vision.core import *
-from fastai.vision.utils import *
 from io import BytesIO
 from starlette.applications import Starlette
 from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import JSONResponse
-from core.integratedclassifier import IntegratedClassifier
-from core.georaster import KGRaster, EluRaster
-from core.observation_factory import ObservationFactory
+from setup import setup
+from fastai.vision.core import PILImage
 from core.helpers import *
 
-disk = '/var/data/'
-download_url = "https://nyc3.digitaloceanspaces.com/inciteful/fungid/v0.4/"
-model_file_name = 'image-model.pkl'
-kg_file_name = 'kg.tif'
-elu_file_name = 'elu.tif'
-db_file_name = 'db.sqlite'
-
-to_download_files = [
-    model_file_name,
-    kg_file_name,
-    elu_file_name,
-    db_file_name,
-]
-
-to_download_files = [(download_url + f, disk + f) for f in to_download_files]
-
-classes = ['black', 'grizzly', 'teddys']
-
-app = Starlette()
-app.add_middleware(CORSMiddleware, allow_origins=[
-                   '*'], allow_headers=['X-Requested-With', 'Content-Type'])
-
-
-async def download_file(url, dest):
-    dest = Path(dest)
-    if dest.exists():
-        print("found: ", dest)
-        return
-
-    print("downloading: ", dest)
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url) as response:
-            data = await response.read()
-            with open(dest, 'wb') as f:
-                f.write(data)
-
-
-loop = asyncio.get_event_loop()
-
-for file in to_download_files:
-    loop.run_until_complete(download_file(file[0], file[1]))
-
-loop.close()
-
-classifier = IntegratedClassifier(
-    disk + model_file_name, disk + db_file_name, cpu=True)
-obs_factory = ObservationFactory(
-    KGRaster(disk + kg_file_name), EluRaster(disk + elu_file_name, disk + db_file_name))
+classifier, obs_factory = setup()
 
 
 async def parse_images_from_request(form_data):
@@ -78,6 +24,10 @@ async def parse_images_from_request(form_data):
             break
 
     return images
+
+app = Starlette()
+app.add_middleware(CORSMiddleware, allow_origins=[
+                   '*'], allow_headers=['X-Requested-With', 'Content-Type'])
 
 
 @ app.route('/analyze', methods=['POST'])
