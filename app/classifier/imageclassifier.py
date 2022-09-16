72fecec77710b65f98b5920b3eef02d5e6bdf44a
Michael Weishuhn <weishuhn@gmail.com>
2022-09-16 14:23:32 +0100

    Moved to batch image prediction

--- app/classifier/imageclassifier.py
+++ app/classifier/imageclassifier.py
@@ -1,23 +1,25 @@
-from fastai.vision.all import load_learner, PILImage
+from fastai.vision.all import load_learner, PILImage, Learner
 from typing import Tuple
 import pandas as pd
 
 
 class ImageClassifier:
     def __init__(self, filename: str, cpu=True):
-        self.learner = load_learner(filename, cpu=cpu)
+        self.learner: Learner = load_learner(filename, cpu=cpu)
         self.vocab = pd.DataFrame(self.learner.dls.vocab, columns=[
                                   "species"]).set_index("species")
 
     def get_predictions(self, images: list[PILImage]) -> Tuple[pd.Series, pd.DataFrame]:
-        df = pd.DataFrame(
-            pd.concat([self._get_prediction(image, str(i))
-                      for i, image in enumerate(images)], axis=1)
-        )
+        df = self._get_predictions(images)
         return df.mean(axis=1).sort_values(ascending=False), df  # type: ignore
 
-    def _get_prediction(self, image: PILImage, idx: str) -> pd.Series:
-        _, _, probs = self.learner.predict(image)
+    def _get_predictions(self, images: list[PILImage]) -> pd.DataFrame:
+        test_dl = self.learner.dls.test_dl(images, num_workers=0)
+
+        preds, _ = self.learner.get_preds(dl=test_dl, reorder=False)
         val = self.vocab.copy()
-        val[idx] = probs
-        return val[idx]
+        for idx, pred in enumerate(preds):  # type: ignore
+            val[idx] = pred
+
+        return val
+ 
